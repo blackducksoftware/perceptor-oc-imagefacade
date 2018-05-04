6a5cda0103ccffa93cbcb17c64b7821d8d355a29
sheppduck <shepp67@gmail.com>
2018-05-04 17:18:45 -0400

    WIP: rearranged the file structure, moved shit around

--- hackathon/docker-entrypoint.sh
+++ hackathon/docker-entrypoint.sh
@@ -1,40 +1,32 @@
 #!/bin/bash
 set -x
 # set -e
-# Some hard coded shit...
+
 OC_SERVER="18.218.176.19"
 OC_UN="clustadm"
 OC_PW="devops123!"
-# We could set the below variable OC_PROJECT via the ARRAY_OF_NS and the for loop below it possibly
 OC_PROJECT="myhub"
 OC_CP_TMP="/tmp"
 OC_TAR_DIR="/temp/hackathon2018"
+NS=$1
+PODS=$2
+OC_OUT_DIR=$3
+WEBAPP="$(oc get pods | grep webapp | cut -d ' ' -f1)"
+echo "$WEBAPP"
+SOLR="$(oc get pods | grep solr | cut -d ' ' -f1)"
+echo "$SOLR"
+# Get all Namespaces and stuff then into an array, trim the first line off the oc get
+ARRAY_OF_NS=(`oc get ns | cut -d ' ' -f1 | awk '{if(NR>1)print}'`)
 # switch to the myhub project run oc to get PODS from myhub ns grep for solr
 # and save output as a variable
 oc login $OC_SERVER:8443 --username=$OC_UN --password=$OC_PW --insecure-skip-tls-verify
 oc project $OC_PROJECT
 ## Grep for and add some add'l HUB PODs - how to use $1, $2, etc. for POD name vars?
-# Get all Namespaces and stuff into an array, trim the first line off the oc get
-ARRAY_OF_NS=(`oc get ns | cut -d ' ' -f1 | awk '{if(NR>1)print}'`)
 # Loop through the array and spit out all the projects
-# Could be super useful instead of hard coding solr and webapp
 for i in "${ARRAY_OF_NS[@]}"
 do
   echo "$i is an oc project"
 done
-
-# Namespaces variable
-NS=$1
-# POD variable
-PODS=$2
-# What directory to output tarball
-OC_OUT_DIR=$3
-# Some hard coded containers...
-WEBAPP="$(oc get pods | grep webapp | cut -d ' ' -f1)"
-echo "$WEBAPP"
-SOLR="$(oc get pods | grep solr | cut -d ' ' -f1)"
-echo "$SOLR"
-
 # now let's copy some files out of the hub-solr container
 # oc cp usage is 'oc cp <namespace/project>/POD:files_to_copy_from_pod /save/files/to/local/dir'
 # ... flesh this part out ... for other containers, get /bin of any test container...
