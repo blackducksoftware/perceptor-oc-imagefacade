3a764ad8876820b34f180dc8b1063ac2166f8dab
Jay Vyas <jay@apache.org>
2018-05-06 13:44:42 -0400

    tweaks

--- pkg/imagefacadeos/config.go
+++ pkg/imagefacadeos/config.go
@@ -35,8 +35,7 @@ func GetConfig() (*Config, error) {
 	var config *Config
 
 	viper.SetConfigName("perceptor_imagefacade_conf")
-	viper.AddConfigPath("/etc/oc-imagefacade")
-	viper.AddConfigPath("etc/oc-imagefacade")
+
 	viper.AddConfigPath("/etc/perceptor_imagefacade")
 
 	err := viper.ReadInConfig()
